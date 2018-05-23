986e88485931cf996ee1d9d614ac5c5ff2690c78
Lavigarg <Lavigarg999@gmail.com>
2018-05-23 19:01:05 +0530

    Initial commit

---
+++ app/src/main/java/lavi/androidapplication/MainActivity.java
@@ -0,0 +1,31 @@
+package lavi.androidapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        webView=(WebView)findViewById(R.id.webview);
+        webView.loadUrl("Enter Your Url Here");
+        WebSettings webSettings = webView.getSettings();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack())
+        {webView.goBack();}
+        else{
+
+            super.onBackPressed();
+        }}
+}
+
